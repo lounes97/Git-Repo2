3e72e6a35d575c84cb3ec525816dcd7afa45f570
Lounes_Cydeo <zizoulounes@gmail.com>
2023-01-23 21:01:08 -0600

    lion class created

---
+++ src/firstPack/Lion.java
@@ -0,0 +1,17 @@
+package firstPack;
+
+public class Lion extends src.firstPack.Animal implements WildAnimal{
+    public Lion(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Lion "+getName()+" is eating buffalo");
+    }
+
+    @Override
+    public void hunt() {
+
+    }
+}
